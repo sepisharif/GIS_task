b7b80526c3964d0e67b4116b62fdf9c706499f7e
Sepideh Sharif <s_sharif996@yahoo.com>
2023-01-12 14:43:37 +1300

    updated the task

--- Crow-files2-task.R
+++ Crow-files2-task.R
@@ -1,9 +1,42 @@
 
-remotes::install_github("rstudio/htmltools")
-library(sf)
-library(mapview)
+#The example of GIS data (specifically North Carolina, NC)which includes the longitude and latitude on the map around the world by clicking
 
-starbucksNC <- read.csv("https://raw.githubusercontent.com/libjohn/mapping-with-R/master/data/All_Starbucks_Locations_in_the_US_-_Map.csv")
+install.packages("mapview")
+install.packages('dplyr')
+library(mapview)
+library(sf)
+library (dplyr)
+starbucksNC <-read.csv("https://raw.githubusercontent.com/libjohn/mapping-with-R/master/data/All_Starbucks_Locations_in_the_US_-_Map.csv")
 
 mapview(starbucksNC, xcol = "Longitude", ycol = "Latitude", crs = 4269, grid = FALSE)
 
+#Install & load  (geosphere), the  package for the geographic distance between two points with latitude/longitude coordinates
+
+install.packages("geosphere")             
+library("geosphere")
+
+## Note Latitude designates North and South of the Equator. Longitude designates East and West of the Prime Meridian.
+## Note A negative latitude means South of the Equator, and a negative longitude means West of the Prime Meridian.
+
+# Christchurch & Dunedin longitude and Latitude
+my_points <- matrix(c( 172.54028,  170.41992,
+                       -43.54457,-45.88818)
+                    ,nrow = 2)
+colnames(my_points) <- c("Longitude","Latitude")
+
+######################################################################################################
+my_dist1 <- distHaversine(my_points, r=6378137)          # Calculate Haversine distance
+my_dist1 # it is m therefore it should divide by 1000 to change to km 
+Chch_Dunedin1=my_dist1/1000
+Chch_Dunedin1
+#######################################################################################
+my_dist2 <- distVincentyEllipsoid(my_points, a=	6378137, b=6356752.3142, f=1/298.257223563)    # Calculate Vincenty ellipsoid distance
+my_dist2   # it is m therefore it should divide by 1000 to change to km
+Chch_Dunedin2=my_dist2/1000
+Chch_Dunedin2
+###########################################################
+my_dist3 <- distGeo(c(172.54028,-43.54457 ), c(170.41992, -45.88818), a=6378137, f=1/298.257223563)    # Calculate the geodesic WGS84
+my_dist3  # it is m therefore it should divide by 1000 to change to km
+Chch_Dunedin3=my_dist3/1000
+Chch_Dunedin3
+##############
